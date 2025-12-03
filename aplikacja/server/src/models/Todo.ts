8b9c200d5352743f634e3f18fae9ae5dff9865dc
Adam Małysz <adam.malysz3@onet.eu>
2025-12-03 02:43:54 +0100

    added database

--- aplikacja/server/src/models/Todo.ts
+++ aplikacja/server/src/models/Todo.ts
@@ -1,1 +1,24 @@
 import { Schema, model } from 'mongoose';
+//1, definicja interfejsu (tp)
+//mongoose będzie dbał o wartości, ale ten interfejs dba o typ w kodzie ts.
+interface ITodo {
+    title: string;
+    description: string;
+    dueDate?: Date;//? oznacza, że pole jest opcjonalne
+    isCompleted: boolean;
+}
+//2. definicja schematu
+const todoSchema = new Schema<ITodo>({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: { type: String, required: false },
+    dueDate: { type: Date, required: false },
+    isCompleted: { type: Boolean, default: false },
+
+},
+    { timestamps: true }); //zapisywanie czasu utworzenia i edycji
+//3. zdefiniowanie modelu
+export const Todo = model<ITodo>('Todo', todoSchema);
